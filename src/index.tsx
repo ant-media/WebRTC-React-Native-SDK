72aa9ed5cd67350555fe97d707a7df2ae7a2acac
Mustafa BOLEKEN <mustafaboleken@yahoo.com>
2023-12-04 13:18:57 +0300

    Add all tracks

--- src/index.tsx
+++ src/index.tsx
@@ -250,7 +250,7 @@ export function useAntMedia(params: Params) {
         if (!playStreamIds.includes(streamId) && localStream.current) {
           // @ts-ignore
           localStream.current.getTracks().forEach((track) => {
-            remotePeerConnection[streamId].addTrack(track, localStream.current);
+            localStream.current.getTracks().forEach((track: MediaStreamTrack) => { remotePeerConnection[streamId].addTrack(track, localStream.current); });
           });
         }
 
