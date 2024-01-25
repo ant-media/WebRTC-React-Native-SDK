ee882ffe28cf934e2e9c02104ee843a6ea3d7851
USAMAWIZARD <usamawizard@gmail.com>
2024-01-25 11:27:50 +0530

    rename conference

--- samples/Conference/index.tsx
+++ samples/Conference/index.tsx
@@ -1,5 +1,5 @@
 import {AppRegistry} from 'react-native';
-import App from './src/multitrackConference';
+import App from './src/Conference';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
